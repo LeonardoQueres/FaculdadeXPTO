8763de95db505cd432fd704b87d866e3c2890ab6
leonardo <leonardoqueres@hotmail.com>
2018-05-25 09:40:52 -0300

    project

---
+++ CrossCutting.IoC/Configuracao.cs
@@ -0,0 +1,52 @@
+﻿using Aplication.AppService;
+using Aplication.Interface;
+using Domain.Interface.Repository;
+using Domain.Interface.Service;
+using Domain.Service;
+using Infra.Data;
+using Infra.Data.Repository;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CrossCutting.IoC
+{
+    public class Configuracao
+    {
+        public static void InjecaoDependencia(IServiceCollection services)
+        {
+            #region Aluno
+            services.AddScoped<IAlunoAppService, AlunoAppService>();
+            services.AddScoped<IAlunoService, AlunoService>();
+            services.AddScoped<IAlunoRepository, AlunoRepository>();
+            #endregion
+
+            #region Disciplina
+            services.AddScoped<IDisciplinaAppService, DisciplinaAppService>();
+            services.AddScoped<IDisciplinaService, DisciplinaService>();
+            services.AddScoped<IDisciplinaRepository, DisciplinaRepository>();
+            #endregion
+
+            #region Turma
+            services.AddScoped<ITurmaAppService, TurmaAppService>();
+            services.AddScoped<ITurmaService, TurmaService>();
+            services.AddScoped<ITurmaRepository, TurmaRepository>();
+            #endregion
+
+            #region TurmaAluno
+            services.AddScoped<ITurmaAlunoService, TurmaAlunoService>();
+            services.AddScoped<ITurmaAlunoRepository, TurmaAlunoRepository>();
+            #endregion
+
+            #region TurmaDisciplina
+            services.AddScoped<ITurmaDisciplinaService, TurmaDisciplinaService>();
+            services.AddScoped<ITurmaDisciplinaRepository, TurmaDisciplinaRepository>();
+            #endregion
+
+            #region ValidarCpf
+            services.AddScoped<IValidarCpfAppService, ValidarCpfAppService>();
+            services.AddScoped<IValidarCpfService, ValidarCpfService>();
+            #endregion
+
+            services.AddScoped<AdminContext>();
+        }
+    }
+}
